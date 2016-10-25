96300f1ca27f4be5370d4ed81cc893e834968363
Eugene Aslanov <aslanov.ev@gmail.com>
2016-10-25 22:00:41 +0400

    Add missing (t) for l, now solver can finish solving

--- task1.py
+++ task1.py
@@ -26,7 +26,7 @@ v_1_2 = (v_1_x ** 2 + v_1_y ** 2)
 alpha = phi(t) - pi / 2 + theta(t)
 r_2_x = r_1_x + l(t) * sin(alpha)
 v_2_x = diff(r_2_x, t)
-r_2_y = r_1_y - l * cos(alpha)
+r_2_y = r_1_y - l(t) * cos(alpha)
 v_2_y = diff(r_2_y, t)
 v_2_2 = (v_2_x ** 2 + v_2_y ** 2)
 T = (m_1 * v_1_2 / 2 + m_2 * v_2_2 / 2)
@@ -34,12 +34,14 @@ Pe = (
     -m_1 * µ / sqrt(r_1_x ** 2 + r_1_y ** 2) - m_2 * µ / sqrt(r_2_x ** 2 + r_2_y ** 2) + c / 2.0 * (l(t) - l_0) ** 2)
 L = (T - Pe)
 left_phi = ((diff((diff(L, diff(phi(t), t))), t) - (diff(L, phi(t)))).subs(diff(r(t), t), 0).subs(
-    diff(diff(theta(t), t), t), 0).subs(diff(theta(t), t), sqrt(µ / r(t) ** 3)).subs(r(t), R_earth.value + H))
+    diff(diff(theta(t), t), t), 0).subs(diff(theta(t), t), sqrt(µ / r(t) ** 3)).subs(r(t), R_earth.value + H)
+            .subs(Derivative(0, t), 0))
 left_l = ((diff((diff(L, diff(l(t), t))), t) - (diff(L, l(t)))).subs(diff(r(t), t), 0).subs(
-    diff(diff(theta(t), t), t), 0).subs(diff(theta(t), t), sqrt(µ / r(t) ** 3)).subs(r(t), R_earth.value + H))
+    diff(diff(theta(t), t), t), 0).subs(diff(theta(t), t), sqrt(µ / r(t) ** 3)).subs(r(t), R_earth.value + H)
+          .subs(Derivative(0, t), 0))
 
 print(left_phi)
 print(left_l)
-# print(solve([left_phi, left_l], diff(phi(t), t, t), diff(l(t), t, t)))
+print(solve([left_phi, left_l], diff(phi(t), t, t), diff(l(t), t, t)))
 
 print("Elapsed {} seconds".format(time.time() - start))
