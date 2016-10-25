892cb65b3ea5cf04948abe8c48a33e2de5527040
Eugene Aslanov <aslanov.ev@gmail.com>
2016-10-25 20:33:31 +0400

    Add some calculations

--- task1.py
+++ task1.py
@@ -3,6 +3,10 @@ from sympy import *
 from scipy.constants import G
 from astropy.constants import M_earth, R_earth
 from astropy.units.si import sday
+from sympy.utilities.lambdify import lambdify
+import time
+
+start = time.time()
 
 omega_earth = 2 * np.pi / sday.si.scale
 M_e = M_earth.value
@@ -18,18 +22,24 @@ r_1_x = r(t) * cos(theta(t))
 v_1_x = diff(r_1_x, t)
 r_1_y = r(t) * sin(theta(t))
 v_1_y = diff(r_1_y, t)
-v_1_2 = simplify(v_1_x ** 2 + v_1_y ** 2)
+v_1_2 = (v_1_x ** 2 + v_1_y ** 2)
 alpha = phi(t) - pi / 2 + theta(t)
 r_2_x = r_1_x + l(t) * sin(alpha)
 v_2_x = diff(r_2_x, t)
 r_2_y = r_1_y - l * cos(alpha)
 v_2_y = diff(r_2_y, t)
-v_2_2 = simplify(v_2_x ** 2 + v_2_y ** 2)
-T = simplify(m_1 * v_1_2 / 2 + m_2 * v_2_2 / 2)
-Pe = simplify(
+v_2_2 = (v_2_x ** 2 + v_2_y ** 2)
+T = (m_1 * v_1_2 / 2 + m_2 * v_2_2 / 2)
+Pe = (
     -m_1 * µ / sqrt(r_1_x ** 2 + r_1_y ** 2) - m_2 * µ / sqrt(r_2_x ** 2 + r_2_y ** 2) + c / 2.0 * (l(t) - l_0) ** 2)
-L = simplify(T - Pe)
-print((diff(simplify(diff(L, diff(phi(t), t))), t) - (diff(L, phi(t)))).subs(diff(r(t), t), 0).subs(
+L = (T - Pe)
+left_phi = ((diff((diff(L, diff(phi(t), t))), t) - (diff(L, phi(t)))).subs(diff(r(t), t), 0).subs(
     diff(diff(theta(t), t), t), 0).subs(diff(theta(t), t), sqrt(µ / r(t) ** 3)).subs(r(t), R_earth.value + H))
-print((diff(simplify(diff(L, diff(l(t), t))), t) - (diff(L, l(t)))).subs(diff(r(t), t), 0).subs(
+left_l = ((diff((diff(L, diff(l(t), t))), t) - (diff(L, l(t)))).subs(diff(r(t), t), 0).subs(
     diff(diff(theta(t), t), t), 0).subs(diff(theta(t), t), sqrt(µ / r(t) ** 3)).subs(r(t), R_earth.value + H))
+
+print(left_phi)
+print(left_l)
+# print(solve([left_phi, left_l], diff(phi(t), t, t), diff(l(t), t, t)))
+
+print("Elapsed {} seconds".format(time.time() - start))
