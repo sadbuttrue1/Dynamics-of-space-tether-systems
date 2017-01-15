6251c87809b09b3c9039712b65362c720adc8644
Eugene Aslanov <aslanov.ev@gmail.com>
2017-01-15 23:10:28 +0400

    Fix kinetic energy for cable

--- task2.py
+++ task2.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 
 start = time.time()
 
+omega_earth = 2 * np.pi / sday.si.scale
 M_e = M_earth.value
 µ = G * M_e
 
@@ -24,10 +25,11 @@ r_c = Matrix([R * sin(phi(t)), R * cos(phi(t))])
 r_c_n = sqrt(r_c[0] ** 2 + r_c[1] ** 2)
 v_c = diff(r_c, t)
 v_c_n = sqrt(v_c[0] ** 2 + v_c[1] ** 2)
+I_c = m_c * h ** 2 / 12
 r_e = Matrix([(V_e * t + R_earth.value) * sin(phi(t)), (V_e * t + R_earth.value) * cos(phi(t))])
 r_e_n = sqrt(r_e[0] ** 2 + r_e[1] ** 2)
 
-T = m_c * v_c_n ** 2 / 2 + m_e * V_e ** 2 / 2
+T = m_c * v_c_n ** 2 / 2 + I_c * omega_earth ** 2 / 2 + m_e * V_e ** 2 / 2
 Pe = -µ * m_c / r_c_n - m_e * µ / r_e_n
 L = simplify(T - Pe)
 print(L)
